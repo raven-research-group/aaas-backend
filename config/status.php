cd46102e71356351e8fa21cccf711cdb3321d774
obengkofijames <obengkofijames@gmail.com>
2024-12-31 11:08:56 +0000

    chore: define status in config

---
+++ config/status.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'success' => [
+        'code' => 200,
+        'message' => 'Success'
+    ],
+
+    'error' => [
+        'code' => 400,
+        'message' => 'Error'
+    ],
+
+    'pending' => [
+        'code' => 411,
+        'message' => 'Pending'
+    ]
+    
+];
