d26712482108e04394b5d8a115c0eba6441e148a
obengkofijames <obengkofijames@gmail.com>
2024-12-31 13:17:58 +0000

    chore: branch  update

--- config/requestBody.php
+++ config/requestBody.php
@@ -4,6 +4,15 @@ return [
 
     'user' => [
         'type' => 'Admin,Standard'
-    ]
+    ],
+
+    'passwordRegex' => [
+        'message' => 'The password must contain at least 8 characters, including at least one uppercase letter, one number, and one special character (e.g., @, $, !, %, *, ? or &).'
+    ],
+
+    'passwordMin' => [
+        'length' => 8,
+        'message' => 'The password must be at least 8 characters long.'
+    ],
 
 ];
