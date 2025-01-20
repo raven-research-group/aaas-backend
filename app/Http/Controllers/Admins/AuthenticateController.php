5f5199fe7d76513991758e96fe8c1522fce6d797
obengkofijames <obengkofijames@gmail.com>
2025-01-20 22:46:14 +0000

    feat: admin sign up and login functionality

---
+++ app/Http/Controllers/Admins/AuthenticateController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Admins;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminLoginRequest;
+use App\Http\Requests\AdminSignupRequest;
+use App\Models\Admin;
+use App\Models\Organization;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticateController extends Controller
+{
+    //
+
+    public function register(AdminSignupRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+
+        return DB::transaction(function () use ($validatedData) {
+            $organization = Organization::firstOrCreate(
+                ['name' => $validatedData['organization_name']],
+                [
+                    'primary_contact' => $validatedData['primary_contact'],
+                    'secondary_contact' => $validatedData['secondary_contact'] ?? null,
+                ]
+            );
+            $admin = Admin::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => bcrypt($validatedData['password']),
+                'organization_id' => $organization->id,
+            ]);
+
+            return response()->json([
+                'status' => config('status.success.code'),
+                'message' => 'Signup successful',
+                'data' => [
+                    'admin' => $admin,
+                    'organization' => $organization,
+                ],
+            ]);
+        });
+    }
+
+
+
+    public function login(AdminLoginRequest $request): JsonResponse
+    {
+        $validatedData = $request->validated();
+        $admin = Admin::where('email', $validatedData['email'])->first();
+
+        if ($admin && Hash::check($validatedData['password'], $admin->password)) {
+            return response()->json([
+                'status' => config('status.success.code'),
+                'message' => 'Login successful',
+                'data' => [
+                    'admin' => $admin,
+                ],
+            ]);
+        }
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Invalid credentials',
+        ]);
+    }
+}
