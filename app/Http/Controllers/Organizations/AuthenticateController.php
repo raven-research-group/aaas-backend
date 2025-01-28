adb5d63d59d61201875176138b99a09ebdcfe0c3
obengkofijames <obengkofijames@gmail.com>
2025-01-28 10:01:37 +0000

    chore : validate request body

--- app/Http/Controllers/Organizations/AuthenticateController.php
+++ app/Http/Controllers/Organizations/AuthenticateController.php
@@ -6,33 +6,35 @@ use App\Http\Controllers\Controller;
 use App\Models\Organization;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Traits\ApiResponseTrait;
+use App\Traits\RequestValidationTrait;
 
 class AuthenticateController extends Controller
 {
     //
-    public function token(Request $request){
+    use ApiResponseTrait;
+    use RequestValidationTrait;
 
-        $creds = $request->only("name","api_secret");
-        Log::info(json_encode($creds));
 
-        $organization = Organization::where("name",$creds["name"])->where("api_secret",$creds["api_secret"])->first();
+    public function token(Request $request)
+    {
 
-        if(!$organization){
-            return response()->json([
-                "message" => "Invalid credentials"
-            ],401);
+        $validation = $this->validateOrganizationGetToken($request);
+        if ($validation !== true) {
+            return $validation;
+        }
+        $creds = $request->only("name", "api_secret");
+
+        $organization = Organization::where("name", $creds["name"])->where("api_secret", $creds["api_secret"])->first();
+
+        if (!$organization) {
+            return $this->errorResponse("Invalid credentials");
         }
 
         $token = $organization->createToken('frontend')->accessToken;
-
-        return response()->json([
-            "message" => "Token generated successfully",
-            "data" => [
-                "token" => $token
-            ]
-        ]);
-
+        $data = [
+            "token" => $token
+        ];
+        return $this->successResponse("Token generated successfully", $data);
     }
-
-
 }
